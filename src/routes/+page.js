1a2f39c19dc23578144b379aed7652e9d1f758a9
evariste1963 <evariste@hotmail.co.uk>
2023-01-18 22:03:23 +0000

    +page.js  fetch(localhost corrected for new pc

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,5 @@
 export async function load({ fetch }) {
-	const res = await fetch('http://127.0.0.1:5173/api/developers');
+	const res = await fetch('http://localhost:5173/api/developers');
 	if (res.ok) {
 		const data = await res.json();
 
